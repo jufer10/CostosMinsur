064c3edca33612cfe0046d97ce296774b178276c
jufer10 <fcarrasco023@gmail.com>
2025-03-26 10:19:12 -0500

    feat

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import streamlit as st
 from components.grafica_principal import grafica_principal
 from components.tabla_principal import tabla_principal
 
+st.set_page_config(layout="wide")
 grafica_principal()
 tabla_principal()
